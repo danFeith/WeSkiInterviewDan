b470059009f08be49b741b42532ba4df30f0b6e6
dan feithlicher <d.feithlicher@gmail.com>
2023-08-02 15:58:35 +0300

    build

--- Client/src/config.tsx
+++ Client/src/config.tsx
@@ -1,2 +1,2 @@
-export const API_URL: string = "http://localhost:4040/api"
-export const WS_URL: string = "ws://localhost:4040"
+export const API_URL: string = "http://15.236.211.37:4040/api"
+export const WS_URL: string = "ws://15.236.211.37:4040"

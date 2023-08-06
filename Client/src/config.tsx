63af8fa1eaf5ec6c17cfdd60b4ac2ac68bc7968f
dan feithlicher <d.feithlicher@gmail.com>
2023-08-06 12:13:43 +0300

    change portS

--- Client/src/config.tsx
+++ Client/src/config.tsx
@@ -1,2 +1,2 @@
-export const API_URL: string = "http://13.39.54.186:4040/api"
-export const WS_URL: string = "ws://13.39.54.186:4040"
+export const API_URL: string = "http://13.39.54.186:80/api"
+export const WS_URL: string = "ws://13.39.54.186:80"
